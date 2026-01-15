af84665a3749c7de4292daff54e1d2d0d10e3fba
chaoul47 <chaoulassaad607@gmail.com>
2026-01-15 18:55:01 +0200

    ceaser

---
+++ caeser.py
@@ -0,0 +1,22 @@
+import string
+
+def caesar_cipher(text, shift, mode):
+    result = ""
+    if mode == "decrypt":
+        shift = -shift
+
+    for char in text:
+        if char in string.ascii_uppercase:
+            shifted_index = (ord(char) - ord('A') + shift) % 26
+            result += chr(shifted_index + ord('A'))
+        else:
+            result += char
+    return result
+
+
+text = input("Enter message (UPPERCASE): ")
+shift = int(input("Enter shift key: "))
+mode = input("Enter mode (encrypt/decrypt): ").lower()
+
+output = caesar_cipher(text, shift, mode)
+print("Result:", output)
